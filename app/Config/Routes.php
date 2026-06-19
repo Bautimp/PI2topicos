d2264c4938205eef507a8e8c1d33c8073bc990d2
Bautimp <bautistamontesp@gmail.com>
2026-06-19 19:13:12 -0300

    Implementación del borrado de fotos de un vehículo

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,6 +33,7 @@ $routes->group('', ['filter' => 'authGuard'], function($routes) {
         $routes->get('vehiculos/crear', 'VehiculoController::crear');
         $routes->post('vehiculos/guardar', 'VehiculoController::guardar');
         $routes->get('vehiculos/editar/(:num)', 'VehiculoController::editar/$1');
+        $routes->get('vehiculos/eliminar-imagen/(:num)/(:num)', 'VehiculoController::eliminarImagen/$1/$2');
         $routes->post('vehiculos/actualizar/(:num)', 'VehiculoController::actualizar/$1');
         $routes->get('vehiculos/baja/(:num)', 'VehiculoController::bajaLogica/$1');
 
