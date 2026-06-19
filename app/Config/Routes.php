bed00ec0e4c08ff0543e41dc43367909b80ec290
Bautimp <bautistamontesp@gmail.com>
2026-06-18 21:18:42 -0300

    Cambio en la ruta básica del sitio "/"

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,7 +3,7 @@
 use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'AuthController::login');
 $routes->get('login', 'AuthController::login');
 $routes->post('login/procesar', 'AuthController::procesarLogin');
 $routes->get('catalogo', 'VehiculoController::catalogo');
